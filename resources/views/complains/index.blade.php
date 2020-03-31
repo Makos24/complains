e837fec98dc277bd2d34bd65c753140081204094
root <root@PGID1.localdomain>
2020-03-31 13:08:50 +0100

    summary for user

--- resources/views/complains/index.blade.php
+++ resources/views/complains/index.blade.php
@@ -6,6 +6,55 @@
 
 
 <main class="bg-white-300 flex-1 p-3 overflow-hidden">
+    @if(auth()->user()->hasRole('User'))
+
+ <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2">
+            <div class="shadow-lg bg-red-300 border-l-8 hover:bg-red border-red-vibrant-dark mb-2 p-2 md:w-1/4 mx-2">
+                <div class="p-4 flex flex-col">
+                    <a href="#" class="no-underline text-white text-2xl">
+                        {{App\Complain::all()->count()}}
+                    </a>
+                    <a href="#" class="no-underline text-white text-lg">
+                        Complains Filed Overall
+                    </a>
+                </div>
+            </div>
+
+            <div class="shadow bg-blue-400 border-l-8 hover:bg-blue-400 border-info-dark mb-2 p-2 md:w-1/4 mx-2">
+                <div class="p-4 flex flex-col">
+                    <a href="#" class="no-underline text-white text-2xl">
+                        {{App\Complain::whereDate('created_at', \Carbon\Carbon::today())->count()}}
+                    </a>
+                    <a href="#" class="no-underline text-white text-lg">
+                        Complains Filed Today
+                    </a>
+                </div>
+            </div>
+
+            <div class="shadow bg-yellow-400 border-l-8 hover:bg-warning-dark border-warning-dark mb-2 p-2 md:w-1/4 mx-2">
+                <div class="p-4 flex flex-col">
+                    <a href="#" class="no-underline text-white text-2xl">
+                         {{App\Complain::where('status', 0)->count()}}
+                    </a>
+                    <a href="#" class="no-underline text-white text-lg">
+                        Pending Complains
+                    </a>
+                </div>
+            </div>
+
+            <div class="shadow bg-green-400 border-l-8 hover:bg-success-dark border-success-dark mb-2 p-2 md:w-1/4 mx-2">
+                <div class="p-4 flex flex-col">
+                    <a href="#" class="no-underline text-white text-2xl">
+                        {{App\Complain::where('status', 1)->count()}}
+                    </a>
+                    <a href="#" class="no-underline text-white text-lg">
+                        Complains Resolved
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endif
+
     <div class="flex flex-col">
 
         <div x-data="{ 'isDialogOpen': false, 'isOpen': false }" @open-modal.window="isOpen = true" @close-modal.window="isDialogOpen = false" @keydown.escape="isDialogOpen = false" @closee-modal.window="isOpen = false" class="break-words bg-white border border-2 rounded shadow-md">
